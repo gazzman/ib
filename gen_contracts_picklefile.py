ea858ae3984c44c1f52723d68a2f018d5b4d9299
gazzman <sgazzisg@uw>
2012-08-19 22:34:26 -0400

    Used settattr to set the attributes of the Contract fields.

--- gen_contracts_picklefile.py
+++ gen_contracts_picklefile.py
@@ -10,37 +10,16 @@ import csv
 import pickle
 
 import com.ib.client.Contract as Contract
-import java.util.Vector as Vector
-
-headers = OrderedDict()
-headers['conId'] = int()
-headers['symbol'] = str()
-headers['secType'] = str()
-headers['expiry'] = str()
-headers['strike'] = float()
-headers['right'] = str()
-headers['multiplier'] = str()
-headers['exchange'] = str()
-headers['currency'] = str()
-headers['localSymbol'] = str()
-headers['comboLegs'] = Vector()
-headers['primaryExch'] = str()
-headers['includeExpired'] = bool()
-headers['secIdType'] = str()
-headers['secId'] = str()
 
 def gen_picklefile(csvfile, picklefile):
     contract_list = list()
-
     cdata = csv.DictReader(open(csvfile, 'rb'))
     for row in cdata:
-        for header in headers:
-            if header not in row:
-                row[header] = headers[header]
-        ordered_row = map(lambda x: row[x], headers)
-        contract_list.append(Contract(*ordered_row))
+        contract = Contract()
+        for m in dir(contract):
+            if m in row: setattr(contract, m, row[m])
+        contract_list.append(contract)
     pickle.dump(contract_list, open(picklefile, 'wb'), pickle.HIGHEST_PROTOCOL)
-    return contract_list
 
 if __name__ == '__main__':
     desc = ' '.join(['A script to pickle a csv as a list of', 
@@ -52,7 +31,7 @@ if __name__ == '__main__':
                     'in. Otherwise, you may end up using too many resources.'])
 
     p = argparse.ArgumentParser(description=desc)
-    p.add_argument('inputfile', type=str,
+    p.add_argument('inputfile', type=str, 
                    help='A csv of contract data. Not all fields need values.')
 
     p.add_argument('-o', metavar='outputfile', default=None, dest='outputfile', 
