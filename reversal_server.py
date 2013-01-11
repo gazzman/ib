60b2fe70e2c190a7394920a4cef81309dd975165
gazzman <sgazzisg@uw>
2013-01-11 14:20:35 -0500

    Major simplification by allowing IBJts API to handle concurrency.

--- reversal_server.py
+++ reversal_server.py
@@ -7,20 +7,11 @@ import threading
 
 from reversal import Reversal
 
-r = Reversal(client_id=1001)
-
 errmsg = 'Failed to specify long or short for ticker %s, expiry %s, strike %s'
 
-def gen_client_id():
-    d = datetime.now().isoformat().split('T')[-1]
-    d = ''.join(d.split(':'))
-    d = ''.join(d.split('.'))
-    return int(d[:-3])
+r = Reversal(client_id=1001)
 
-class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
-    pass
-
-class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
+class TCPRequestHandler(SocketServer.BaseRequestHandler):
     def handle(self):
         reversal_position = self.request.recv(1024).strip()
         data = [x.strip() for x in reversal_position.split(',')]
@@ -28,12 +19,10 @@ class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
         if long.lower() == 'long': long = True
         elif long.lower() == 'short': long = False
         else: raise Exception(errmsg % (ticker, expiry, strike))
-        client_id = gen_client_id()
         order_id = r.enter_position(ticker, expiry, strike, qty=qty, long=long)
 
 if __name__ == '__main__':
     HOST = sys.argv[1]
     PORT = int(sys.argv[2])
-    server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
+    server = SocketServer.TCPServer((HOST, PORT), TCPRequestHandler)
     server.serve_forever()
-    r.client.disconnect()
