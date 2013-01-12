fb7ed882d5bda4845847ca8e3e65c70d10bcf8bd
gazzman <sgazzisg@uw>
2013-01-12 16:00:39 -0500

    Improved logging and error handling.

--- reversal_server.py
+++ reversal_server.py
@@ -7,19 +7,29 @@ import threading
 
 from reversal import Reversal
 
-errmsg = 'Failed to specify long or short for ticker %s, expiry %s, strike %s'
+errbase = 'for ticker %s, expiry %s, strike %s, qty %s'
+errmsg1 = ' '.join(['Failed to specify long or short', errbase])
+errmsg2 = ' '.join(['%s REVERSAL position NOT entered', errbase])
+infomsg = ' '.join(['%s REVERSAL position entered', errbase,
+                    'with order id %i']) 
 
-r = Reversal(client_id=1001)
+r = Reversal()
 
 class TCPRequestHandler(SocketServer.BaseRequestHandler):
     def handle(self):
         reversal_position = self.request.recv(1024).strip()
         data = [x.strip() for x in reversal_position.split(',')]
-        ticker, expiry, strike, qty, long = data
-        if long.lower() == 'long': long = True
-        elif long.lower() == 'short': long = False
-        else: raise Exception(errmsg % (ticker, expiry, strike))
-        order_id = r.enter_position(ticker, expiry, strike, qty=qty, long=long)
+        ticker, expiry, strike, qty, longshort = data
+        if longshort.lower() == 'long': ls = True
+        elif longshort.lower() == 'short': ls = False
+        else: raise Exception(errmsg1 % (ticker, expiry, strike))
+        oid = r.enter_position(ticker, expiry, strike, qty=qty, longshort=ls)
+        if not oid:
+            r.client.logger.error(errmsg2 % (longshort.upper(), ticker, expiry,
+                                             strike, qty))
+        else:
+            r.client.logger.info(infomsg % (longshort.upper(), ticker, expiry,
+                                            strike, qty, oid))
 
 if __name__ == '__main__':
     HOST = sys.argv[1]
