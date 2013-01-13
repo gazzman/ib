1cca0795583d97ec728c741ead20348df9a6817b
gazzman <sgazzisg@uw>
2013-01-13 02:56:15 -0500

    Improved message handling and SIGINT initiated server shutdowns.

--- reversal_server.py
+++ reversal_server.py
@@ -2,6 +2,7 @@
 from datetime import datetime
 from time import sleep
 import SocketServer
+import signal
 import sys
 import threading
 
@@ -17,22 +18,35 @@ r = Reversal()
 
 class TCPRequestHandler(SocketServer.BaseRequestHandler):
     def handle(self):
-        reversal_position = self.request.recv(1024).strip()
-        data = [x.strip() for x in reversal_position.split(',')]
-        ticker, expiry, strike, qty, longshort = data
-        if longshort.lower() == 'long': ls = True
-        elif longshort.lower() == 'short': ls = False
-        else: raise Exception(errmsg1 % (ticker, expiry, strike))
-        oid = r.enter_position(ticker, expiry, strike, qty=qty, longshort=ls)
-        if not oid:
-            r.client.logger.error(errmsg2 % (longshort.upper(), ticker, expiry,
-                                             strike, qty))
-        else:
-            r.client.logger.info(infomsg % (longshort.upper(), ticker, expiry,
-                                            strike, qty, oid))
+        message = self.request.recv(1024).strip()
+        data = [x.strip() for x in message.split(',')]
+        if len(data) == 5:
+            ticker, expiry, strike, qty, longshort = data
+            if longshort.lower() == 'long': ls = True
+            elif longshort.lower() == 'short': ls = False
+            else: raise Exception(errmsg1 % (ticker, expiry, strike))
+            oid = r.enter_position(ticker, expiry, strike, qty=qty, 
+                                   longshort=ls)
+            if not oid:
+                r.client.logger.error(errmsg2 % (longshort.upper(), ticker, 
+                                                 expiry, strike, qty))
+            else:
+                r.client.logger.info(infomsg % (longshort.upper(), ticker, 
+                                                expiry, strike, qty, oid))
+        elif message == 'CANCEL OPEN ORDERS': r.client.cancel_open_orders()
+        else: raise Exception("Message '%s' not recognized" % message)
 
 if __name__ == '__main__':
     HOST = sys.argv[1]
     PORT = int(sys.argv[2])
     server = SocketServer.TCPServer((HOST, PORT), TCPRequestHandler)
+    r.client.logger.warn('Reversal server started. Listeing on socket %s:%i' 
+                         % (HOST, PORT))
+    def cleanup(signal, frame):
+        server.server_close()
+        r.client.disconnect()
+        r.client.logger.warn('Reversal server shutdown')
+        sys.exit(0)
+    signal.signal(signal.SIGINT, cleanup)
     server.serve_forever()
+
