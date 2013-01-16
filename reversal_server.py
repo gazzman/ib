d81933af0055b3ebdc42c4cb8c75996fbb9b2c6f
gazzman <sgazzisg@uw>
2013-01-15 23:02:41 -0500

    Added capability to use the reversal module's separate-order processing.

--- reversal_server.py
+++ reversal_server.py
@@ -14,8 +14,10 @@ logger = logging.getLogger('ib.client')
 errbase = 'for ticker %s, expiry %s, strike %s, qty %s'
 errmsg1 = ' '.join(['Failed to specify long or short', errbase])
 errmsg2 = ' '.join(['%s REVERSAL position NOT entered', errbase])
-infomsg = ' '.join(['%s REVERSAL position entered', errbase,
-                    'with order id %i']) 
+infomsg1 = ' '.join(['%s REVERSAL position entered', errbase,
+                     'with order id %i']) 
+infomsg2 = ' '.join(['%s REVERSAL position entered', errbase,
+                     'with order ids %i, %i, %i']) 
 
 r = Reversal()
 
@@ -31,17 +33,24 @@ class TCPRequestHandler(SocketServer.BaseRequestHandler):
             oid = r.enter_position(ticker, expiry, strike, qty=qty, 
                                    longshort=ls)
             if not oid:
-                logger.error(errmsg2 % (longshort.upper(), ticker, 
-                                                 expiry, strike, qty))
-            else:
-                logger.info(infomsg % (longshort.upper(), ticker, 
-                                                expiry, strike, qty, oid))
+                logger.error(errmsg2 % (longshort.upper(), ticker, expiry,
+                                        strike, qty))
+            elif type(oid) == int:
+                logger.info(infomsg1 % (longshort.upper(), ticker, expiry,
+                                        strike, qty, oid))
+            elif type(oid) == list:
+                logger.info(infomsg2 % (longshort.upper(), ticker, expiry, 
+                                        strike, qty, oid[0], oid[1], oid[2]))
+            else: logger.error('Something is wrong with the returned order id')
         elif message == 'CANCEL OPEN ORDERS': r.client.cancel_open_orders()
         else: raise Exception("Message '%s' not recognized" % message)
 
 if __name__ == '__main__':
     HOST = sys.argv[1]
     PORT = int(sys.argv[2])
+    if len(sys.argv) == 4:  
+        if sys.argv[3].strip("'").lower() == 'papertrader': r.paper_trader=True
+        else: raise Exception("Arguments are: HOST PORT ['papertrader']")
     server = SocketServer.TCPServer((HOST, PORT), TCPRequestHandler)
     logger.warn('Reversal server started. Listeing on socket %s:%i' 
                          % (HOST, PORT))
