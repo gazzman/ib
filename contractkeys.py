68301e031e0b0acee0aac3b0ae69c2c09d5e317d
gazzman <sgazzisg@uw>
2013-01-21 01:03:46 -0500

    Added a CurrencyLocal class.

--- contractkeys.py
+++ contractkeys.py
@@ -2,7 +2,8 @@
 from collections import namedtuple
 
 ContractId = namedtuple('ContractId', ['m_conId'])
-Currency = namedtuple('Currency', ['m_localSymbol'])
+Currency = namedtuple('Currency', ['m_currency', 'm_symbol'])
+CurrencyLocal = namedtuple('CurrencyLocal', ['m_localSymbol'])
 Option = namedtuple('Option', ['m_symbol', 'm_expiry', 'm_right', 'm_strike'])
 OptionLocal = namedtuple('OptionLocal', ['m_localSymbol'])
 Stock = namedtuple('Stock', ['m_symbol'])
