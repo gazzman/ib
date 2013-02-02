78b3cbca9afc75bc9d3263d27174a407624fc8c7
gazzman <sgazzisg@uw>
2013-02-01 20:30:57 -0500

    A simple script to translate butterfly contract ids to strikes.

---
+++ butterfly_contract_interpreter.py
@@ -0,0 +1,35 @@
+#!/usr/local/bin/jython
+from collections import namedtuple
+import glob
+import os
+import shutil
+import signal
+import sys
+
+from ib.client import Client
+from ib.contractkeys import ContractId
+
+pattern = './Butterfly*.mkt'
+newdirname = '%(symbol)s_%(expiry)s_%(right)s'
+newfilename = '%(strike1)0.1f_%(strike2)0.1f_%(strike3)0.1f.mkt'
+
+if __name__ == "__main__":
+    c = Client(client_id=8)
+    c.connect()
+    filenames = glob.glob(pattern)
+    for filename in filenames:
+        conIds = filename.split('.mkt')[0].split('_')[1:]
+        conIds = [int(x) for x in conIds]
+        strikes = []
+        for conId in conIds:
+            contract = c.request_contract_details(ContractId(conId))[0].m_summary
+            strikes.append(contract.m_strike)
+        ndname = newdirname % {'symbol': contract.m_symbol, 
+                              'expiry': contract.m_expiry,
+                              'right': contract.m_right}
+        if not os.path.exists(ndname): os.makedirs(ndname)
+        nfname = newfilename % {'strike1': strikes[0],
+                                'strike2': strikes[1],
+                                'strike3': strikes[2]}
+        shutil.copyfile(filename, '/'.join([ndname, nfname]))
+    c.disconnect()
