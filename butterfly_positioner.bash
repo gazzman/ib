4d94fd8decf6ea93d25b5996566f5ac2e9e6a8aa
gazzman <sgazzisg@uw>
2013-02-03 21:34:48 -0500

    Script now attempts to compute start, end, and interval.

--- butterfly_positioner.bash
+++ butterfly_positioner.bash
@@ -1,22 +1,28 @@
 #!/bin/bash
-OFILE="butterflies.mkt"
 TFILE="tmp.mkt"
 for d in `ls -l | grep dr | awk '{print $9}'`
 do
     COUNT=0
     SYMBOL=`echo $d | awk -F'_' '{print $1}'`
     cd $d
-    touch $OFILE
     touch $TFILE
-    rm $OFILE
     rm $TFILE
     for f in `ls *_*_*.mkt`
     do
+        if [ $COUNT -eq 0 ]
+        then
+            START=`echo $f | awk -F'_' '{print $1}'`
+        fi
         cat $f | awk "{print \"$COUNT \"\$0}" >> $TFILE
         COUNT=$(($COUNT + 1))
     done
+    END=`echo $f | awk -F '_' '{print $3}' | awk -F'.mkt' '{print $1}'`
+    INTERVAL=`echo "scale = 1; ($END-$START-1)/$COUNT" | bc`
     cat ../$SYMBOL.mkt | awk "{print \"$COUNT \"\$0}" >> $TFILE
+    OFILE=$START'_'$END'_'$INTERVAL'.mkt'
+    touch $OFILE
+    rm $OFILE
     sort -k2 $TFILE >> $OFILE
     rm $TFILE
     cd ..
-done    
+done
